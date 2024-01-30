aa08b3a4d50d6dcecde34f65658fa1091f978c35
Colin <colinzqy@gmail.com>
2024-01-30 17:54:17 +0800

    Switched to facebook business sdk

--- agent.py
+++ agent.py
@@ -4,91 +4,93 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import time
 import requests
-from post import post_to_FB
+from post import post_to_page
 #from transformers import pipeline
 
 load_dotenv()
 
 client = OpenAI(
-    api_key=os.getenv('OPENAI_API_KEY')
+	api_key=os.getenv('OPENAI_API_KEY')
 )
 
 # Set up Hugging Face summarization pipeline
 #summarizer = pipeline("summarization")
 
 def get_interior_design_trends(location="Singapore", BING_API_KEY=os.getenv('BING_API_KEY')):
-    start_time_function = time.time()
+	start_time_function = time.time()
     # Define the base URL for Bing Web Search API
-    api_url = "https://api.bing.microsoft.com/v7.0/search"
+	api_url = "https://api.bing.microsoft.com/v7.0/search"
 
     # Define the query to search for interior design trends
-    query = f"Most reliable interior designer companies {location}"
+	query = f"Most reliable interior designer companies {location}"
 
     # Set up headers for the API request
-    headers = {
-        "Ocp-Apim-Subscription-Key": BING_API_KEY,
-    }
+	headers = {
+		"Ocp-Apim-Subscription-Key": BING_API_KEY,
+	}
 
-    # Set up parameters for the API request
-    params = {
-        "q": query,
-        "count": 50,  # Number of results to retrieve (adjust as needed)
-        "offset": 0,
-        "safeSearch": "Moderate",  # Adjust as needed
-    }
+	# Set up parameters for the API request
+	params = {
+		"q": query,
+		"count": 50,  # Number of results to retrieve (adjust as needed)
+		"offset": 0,
+		"safeSearch": "Moderate",  # Adjust as needed
+	}
 
     # Make a GET request to the Bing Web Search API
-    response = requests.get(api_url, headers=headers, params=params)
+	response = requests.get(api_url, headers=headers, params=params)
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON content of the response
-        data = response.json()
+	# Check if the request was successful (status code 200)
+	if response.status_code == 200:
+		# Parse the JSON content of the response
+		data = response.json()
 
-        # Extract relevant information from the response
-        trends = []
+		# Extract relevant information from the response
+		trends = []
 
-        # Use OpenAI API to pass results to assistant
-        for item in data.get("webPages", {}).get("value", []):
-            content = item.get("snippet", "")  # You can use "name" or other fields based on API response
-            trends.append(content)
-        end_time_function = time.time()
-        time_taken_function = end_time_function - start_time_function
-        print(f"Time taken for function (success): {time_taken_function} seconds")
-        return {"location": location, "trends": trends}
-    else:
-      error_message = f"Failed to retrieve interior design trends. Status code: {response.status_code}"
-      try:
-          error_content = response.json()
-          error_message += f", Error content: {error_content}"
-      except json.JSONDecodeError:
-        pass  # Ignore if response content is not JSON
-      end_time_function = time.time()
-      time_taken_function = end_time_function - start_time_function
-      print(f"Time taken for function (failed): {time_taken_function} seconds")
-      return {"error": error_message}
+		# Use OpenAI API to pass results to assistant
+		for item in data.get("webPages", {}).get("value", []):
+			content = item.get("snippet", "")  # You can use "name" or other fields based on API response
+			trends.append(content)
+		end_time_function = time.time()
+		time_taken_function = end_time_function - start_time_function
+		print(f"Time taken for function (success): {time_taken_function} seconds")
+		return {"location": location, "trends": trends}
+	else: 
+		error_message = f"Failed to retrieve interior design trends. Status code: {response.status_code}"
+		try:
+			error_content = response.json()
+			error_message += f", Error content: {error_content}"
+		except json.JSONDecodeError:
+			pass  # Ignore if response content is not JSON
+		end_time_function = time.time()
+		time_taken_function = end_time_function - start_time_function
+		print(f"Time taken for function (failed): {time_taken_function} seconds")
+		return {"error": error_message}
 
 tools_list = [{
-  "type": "function",
+	"type": "function",
     "function": {
-      "name": "get_interior_design_trends",
-      "description": "Get the top 5 most reliable interior designer companies in Singapore. Provide reasons for choosing these companies.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "location": {"type": "string", "description": "Singapore"}
-        },
-        "required": ["location"]
-      }
+        "name": "get_interior_design_trends",
+        "description": "Get the top 5 most reliable interior designer companies in Singapore. Provide reasons for choosing these companies.",
+    	"parameters": {
+        	"type": "object",
+        	"properties": {
+				"location": {"type": "string", "description": "Singapore"}
+			},
+            "required": ["location"]
+		}
     }
 }]
 
 #create assistant (only need to do this ONCE)
 my_assistant = client.beta.assistants.create(
     name="Test3 Interior Design Expert",
-    instructions="You are an interior design expert in Singapore. Conduct research online to answer questions.",
-     model="gpt-4",
-     tools=tools_list
+	instructions="""You are an interior design expert in Singapore. 
+		Conduct research online to answer questions.
+		Do not include links in your final research.""",
+	model="gpt-4",
+	tools=tools_list
 )
 #print(my_assistant.model_dump_json(indent=2))
 
@@ -107,54 +109,53 @@ thread_message = client.beta.threads.messages.create(
 
 # run assistant
 run = client.beta.threads.runs.create(
-  thread_id=empty_thread.id,
-  assistant_id=my_assistant.id
+	thread_id=empty_thread.id,
+	assistant_id=my_assistant.id
 )
 #print(run.model_dump_json(indent=2))
 
 while True:
-  #retrieve status of the run
-  run_status = client.beta.threads.runs.retrieve(thread_id=empty_thread.id,
-  run_id=run.id)
+	#retrieve status of the run
+	run_status = client.beta.threads.runs.retrieve(thread_id=empty_thread.id, run_id=run.id)
 
-  print(f"Run status: {run_status.status}")
-  if run_status.status == 'completed':
-    messages = client.beta.threads.messages.list(
-      thread_id = empty_thread.id
-    )
-    for msg in messages.data:
-      role = msg.role
-      content = msg.content[0].text.value
-      print(f"{role.capitalize()}: {content}")
-      post_to_FB(content)
-      break
-    end_time = time.time()
-    time_taken = end_time - start_time
-    print(f"Time taken: {time_taken} seconds")
-    break
+	print(f"Run status: {run_status.status}")
+	if run_status.status == 'completed':
+		messages = client.beta.threads.messages.list(
+			thread_id = empty_thread.id
+		)
+		for msg in messages.data:
+			role = msg.role
+			content = msg.content[0].text.value
+			print(f"{role.capitalize()}: {content}")
+			post_to_page(content)
+			break
+		end_time = time.time()
+		time_taken = end_time - start_time
+		print(f"Time taken: {time_taken} seconds")
+		break
 
-  elif run_status.status == 'requires_action':
-    print("Requires action")
-    required_actions = run_status.required_action.submit_tool_outputs.model_dump()
-    print(required_actions)
-    tools_output = []
-    for action in required_actions["tool_calls"]:
-        func_name = action["function"]["name"]
-        arguments = json.loads(action["function"]["arguments"])
-        if func_name == "get_interior_design_trends":
-            output = get_interior_design_trends(location=arguments["location"])
-            tools_output.append({
-                "tool_call_id": action["id"],
-                "output": json.dumps(output)  # Convert output to a JSON string
-            })
-        else:
-            print("Function not found")
-    client.beta.threads.runs.submit_tool_outputs(
-        thread_id=empty_thread.id,
-        run_id=run.id,
-        tool_outputs=tools_output
-    )
-time.sleep(1) # Wait for a second before checking again
+	elif run_status.status == 'requires_action':
+		print("Requires action")
+		required_actions = run_status.required_action.submit_tool_outputs.model_dump()
+		print(required_actions)
+		tools_output = []
+		for action in required_actions["tool_calls"]:
+			func_name = action["function"]["name"]
+			arguments = json.loads(action["function"]["arguments"])
+			if func_name == "get_interior_design_trends":
+				output = get_interior_design_trends(location=arguments["location"])
+				tools_output.append({
+					"tool_call_id": action["id"],
+					"output": json.dumps(output)  # Convert output to a JSON string
+				})
+			else:
+				print("Function not found")
+		client.beta.threads.runs.submit_tool_outputs(
+			thread_id=empty_thread.id,
+			run_id=run.id,
+			tool_outputs=tools_output
+		)
+	time.sleep(1) # Wait for a second before checking again
 
 # messages = client.beta.threads.messages.list(
 #   thread_id=empty_thread.id
--- post.py
+++ post.py
@@ -1,7 +1,123 @@
-from facebook import GraphAPI
+# from facebook import GraphAPI
 import os
+from dotenv import load_dotenv
+import json
+from facebook_business.adobjects.page import Page
+from facebook_business.adobjects.pagepost import PagePost
+from facebook_business.session import FacebookSession
+from facebook_business.api import FacebookRequest, FacebookAdsApi
 
-def post_to_FB(message: str):
-    """Writes `message` to a Facebook page. The page is referred to using the PAGE_ACCESS_TOKEN."""
-    graph = GraphAPI(access_token=os.environ['PAGE_ACCESS_TOKEN'])
-    graph.put_object(parent_object='me', connection_name='feed', message=message)
+
+
+def start_session(access_token=None):
+    """Returns an API instance to make API calls."""
+    # Create session and API instance
+    session = FacebookSession(access_token=access_token)
+    return FacebookAdsApi(session=session)
+
+
+def get_app_token():
+    """Returns the App Access Token"""
+    api = start_session()
+    node_id = "oauth"
+    endpoint = "access_token"
+    request = FacebookRequest(node_id, "GET", endpoint, api=api)
+    params = {
+        "client_id": os.environ["APP_ID"],
+        "client_secret": os.environ["APP_SECRET"],
+        "grant_type": "client_credentials"
+    }
+    request.add_params(params)
+
+    try:
+        # Execute the GET request
+        response = request.execute()
+
+        # Access the response data
+        json_data = response.json()
+        # http_status = response.status()
+        # headers = response.headers()
+
+        # Process the response data as needed
+        # print(f"JSON Data: {json_data}")
+        # print(f"HTTP Status: {http_status}")
+        # print(f"Headers: {headers}")
+
+        return json_data["access_token"]
+
+    except Exception as e:
+        # Handle any exceptions that may occur
+        print(f"Error: {e}")
+        return None
+
+
+def get_user_id(app_id, app_token):
+    """Returns the user id"""
+    api = start_session(access_token=app_token)
+    roles_connection = FacebookRequest(app_id, "GET", "roles", api=api)
+    new_response = roles_connection.execute().json()
+    return new_response["data"][0]["user"]
+
+
+def get_page_token(user_token):
+    """Returns Page Access Token"""
+    api = start_session(access_token=user_token)
+    roles_connection = FacebookRequest("me", "GET", "accounts", api=api)
+    new_response = roles_connection.execute().json()
+    return new_response["data"][0]["access_token"]
+
+
+def get_metadata(access_token):
+    """Returns the metadata of object with `access_token`."""
+    api = start_session(access_token=access_token)
+    request = FacebookRequest("me", "GET", "", api=api)
+    request.add_param("metadata", 1)
+    new_response = request.execute().json()
+    return new_response
+
+
+def post_to_page(message: str):
+    """Writes `message` to a Facebook page. The page is referred to using the `page_token`."""
+    page_token = get_page_token(os.environ["USER_TOKEN"]) 
+    api = start_session(access_token=page_token)
+    request = FacebookRequest("me", "POST", "feed", api=api)
+    request.add_param("message", message)
+    response = request.execute()
+    print(response.json())
+
+# app_token = get_app_token(api)
+# print(app_token)
+
+# page_token = get_page_token(os.environ["USER_TOKEN"])       # Seems like User Access Token must be generated manually from the explorer tool
+# print(page_token)
+
+# metadata = get_metadata(page_token)
+# print(json.dumps(metadata, indent=4))
+
+load_dotenv()
+post_to_page("Hi")
+
+# user_id = get_user_id(os.environ["APP_ID"], app_token)
+# print(user_id)
+
+
+
+
+
+# api = FacebookAdsApi.init(app_id=os.environ["APP_ID"], app_secret=os.environ["APP_SECRET"])
+
+# fields = [
+# ]
+# params = {
+#   'message': 'This is a test value',
+# }
+# print Page(id).create_feed(
+#   fields=fields,
+#   params=params,
+# )
+
+# def post_to_FB(message: str):
+#     """Writes `message` to a Facebook page. The page is referred to using the PAGE_ACCESS_TOKEN."""
+#     graph = GraphAPI(access_token=os.environ['PAGE_ACCESS_TOKEN'])
+#     graph.put_object(parent_object='me', connection_name='feed', message=message)
+
